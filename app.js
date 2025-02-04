568512bede6a262837f0ec90a4027ea4f1c5325a
Robenus Gutetta <robatesfu7@gmail.com>
2025-02-03 19:25:15 -0500

    new files added

--- app.js
+++
@@ -1,7 +0,0 @@
-// Importing required modules
-import express from 'express';
-import nodemon from 'nodemon';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose'; // Importing mongoose for MongoDB connection
-dotenv.config(); // Initialize express app
-const app = express(); // express app instance 
