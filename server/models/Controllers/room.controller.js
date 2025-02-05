651d36ec742b772db0015c0953c76bc2ab06dba8
aleconte1996 <alexleconte11@gmail.com>
2025-02-05 17:39:11 -0500

    update

--- server/models/Controllers/room.controller.js
+++ server/models/Controllers/room.controller.js
@@ -50,4 +50,80 @@ router.post("add", async(req, res) => {
 //request type: Get
 // description: returns a room by id 
 
-router.get()
+router.get("/:id", async (req,res)   =>{
+    try{ 
+        const room = await room.findById(req,params.id);
+        if(!room) {
+            throw new Error("Room not found");
+        }
+        res.json({
+            messsage:"room found sucessfully",
+            room: room
+        });
+    } catch (error){
+        res.status(500).json({
+            message: "error",
+            errorMessage: error.message
+        });
+
+    }
+});
+
+
+//endpoint: localhost:3000/rooms/delete/:id
+// request type: DELETE 
+//description: deletes a room by id
+
+router.delete ("delete/:id", async (req,res) => {
+    try {
+        const deletedRoom = await Room.findByIdAndDelete(req.params.id;
+            if(!deletedRoom) {
+                throw new Error("room not found");
+    
+            }
+            res.json({
+                message: "room deleted sucessfully",
+                room: deletedroom
+            });
+    } catch (error){
+        res.status(500).json({
+            message: "error",
+            errorMessage: error.message
+        });
+    }
+});
+
+//endpoint: localhost:3000/rooms/update/:id
+//request type: PUT 
+// description : updates a room by id
+
+router.put("/update/:id",   async(req,res) =>{
+    try {
+        const { name, description, addedUsers} = req.body;
+        const updatedRoom = await Room.findByIdAndUpdate(
+            req.params.id,
+            {name,
+                description,
+                addedUsers
+
+            },
+            { new: true} //returns updated doc 
+        );
+        if (!updatedRoom) {
+            throw new Error ("Room not found");
+        }
+        res.json({
+            message: "Room updated sucessfully",
+            room: updatedRoom
+        });
+        
+        } catch (error){
+            res.status(500).json({
+                message: "error",
+                errorMessage: error.message
+            });
+    }
+}
+);
+
+module.exports = router;
